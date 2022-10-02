511f198e85fd80357dd4bbc3a7db6d6cc347f090
YuliaPtitca <107066957+YuliaPtitca@users.noreply.github.com>
2022-10-02 15:18:09 +0300

    Update MobileBankApiTestV3.java

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -24,7 +24,7 @@ class MobileBankApiTestV3 {
                 // специализированные проверки - лучше
                 // .contentType(ContentType.JSON)
                 // .body("", hasSize(3))
-                .body("[0].currency", equalTo("RUR"))
+                .body("[0].currency", equalTo("RUB"))
                 .body("[1].currency", equalTo("USD"))
                 .body("[2].currency", equalTo("RUB"))
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
